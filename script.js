780cfd63bc108763a72c465813f858649a88027a
Isabel Silvis <isabel.silvis@august.com.au>
2019-07-10 23:01:26 +1000

    More mucking around

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ function initClicky() {
   const image = document.querySelector(
     '.image'
   );
+  const result = document.querySelector(
+    '.result'
+  );
   let count = 0,
     x = 0,
     y = 0;
@@ -17,41 +20,50 @@ function initClicky() {
     y = Math.floor(Math.random() * 100);
     
     button.setAttribute("style", "left: "+x+"%; top: "+y+"%; transform: translate(-"+x+"%, -"+y+"%);");
+    image.setAttribute("style", "display: block;");
 
     switch (count) {
       case 0:
-        button.innerHTML = "He or she?";
+        button.innerHTML = "What's in my belly?";
+        // image.setAttribute("src", "images/ice-cream.gif");
         break;
       case 1:
-        button.innerHTML = "Phoebe or Phoebo?";
-        image.setAttribute("src", "images/phoebo.png");
+        button.innerHTML = "It's not a food baby. What could it be?";
+        image.setAttribute("src", "images/shock.gif");
         break;
       case 2:
-        button.innerHTML = "His or her arm?";
+        button.innerHTML = "It's a baby! Click to reveal gender";
+        image.setAttribute("src", "images/hooray.gif");
         break;
       case 3:
-        button.innerHTML = "Her or his due date?";
+        button.innerHTML = "Phoebe or Phoebo?";
+        image.setAttribute("src", "images/phoebo.gif");
         break;
       case 4:
         button.innerHTML = "Sir or Madam?";
+        image.setAttribute("src", "images/handstand.gif");
         break;
       case 5:
-        button.innerHTML = "His or her aem?";
+        button.innerHTML = "Click here to find out!";
         break;
       case 6:
-        button.innerHTML = "His or her aem?";
+        button.innerHTML = "Her or his due date?";
+        image.setAttribute("src", "images/christmas.gif");
         break;
       case 7:
-        button.innerHTML = "His or her aem?";
+        button.innerHTML = "His or her arm?";
+        image.setAttribute("src", "images/arm.jpg");
         break;
       case 8:
-        button.innerHTML = "His or her aem?";
+        button.innerHTML = "Click here";
         break;
       case 9:
-        button.innerHTML = "His or her aem?";
+        button.innerHTML = "Click here";
         break;
       default:
-        console.log('Pandas');
+        result.setAttribute("style", "display: block;");
+        button.setAttribute("style", "display: none;");
+        image.setAttribute("style", "display: none;");
     }
 
     count++;
